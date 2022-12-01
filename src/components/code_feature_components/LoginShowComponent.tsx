0aba2ec6beda533703dc10b94210d8a6c2d9a0ce
Aman Mamgain <amanmamgain9@gmail.com>
2022-12-01 15:59:45 +0530

    handle case of public address

--- src/components/code_feature_components/LoginShowComponent.tsx
+++ src/components/code_feature_components/LoginShowComponent.tsx
@@ -5,7 +5,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import Button from '@mui/material/Button';
 
-const codeString = `glipWallet.login('google', window.location.href)`;
+const codeString = `let publicAddress = await glipWallet.login('google', window.location.href)`;
 const ShowConnectModalCodeComponent = () => {
     
     return (
@@ -35,6 +35,7 @@ export default function LoginShowComponent(props:any) {
 
     const handleLogin = async () => {
         let loginResp = await wallet.login('google', window.location.href);
+        alert(`login success ${loginResp}`);
         console.log('loginResp', loginResp);
     }
     return (
